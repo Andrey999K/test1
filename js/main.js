162ec782a2306daf522d73ca9e27ac1acb47e922
Andrey999K <andrey1234xxxx@mail.ru>
2020-09-08 10:39:25 +0300

    08.09.2020 10:39

--- js/main.js
+++ js/main.js
@@ -1,8 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
   const container = document.querySelector(".container");
 
-  const addElement = () => {
-    console.dir(event.target);
+  const addInput = (element) => {
+    container.insertAdjacentHTML(
+      "beforeend",
+      `<input type="text" class="input ${element}">`
+    );
+    document.querySelector(`input.${element}`).focus();
+  };
+
+  const addElement = (className) => {
+    if (className == "title") {
+      tag = "h2";
+    }
+    if (className == "text") {
+      tag = "p";
+    }
+    if (className == "list") {
+      tag = "ul";
+    }
+    if (className == "list_number") {
+      tag = "ol";
+    }
+    let element = document.createElement(tag);
+    if (tag == "ul" || tag == "ol") {
+      let li = document.createElement("li");
+      li.textContent = event.target.value;
+      console.log(li);
+      element.insertAdjacentElement("beforeend", li);
+    } else {
+      element.className = className;
+      element.textContent = event.target.value;
+    }
+    container.insertAdjacentElement("beforeend", element);
+    container.removeChild(event.target);
   };
 
   document.addEventListener("keydown", () => {
@@ -11,22 +42,23 @@ document.addEventListener("DOMContentLoaded", () => {
         event.preventDefault();
       }
       if (event.code == "KeyH") {
-        container.insertAdjacentHTML(
-          "beforeend",
-          `<input type="text" class="input title">`
-        );
-        document.querySelector(".title").focus();
+        addInput("title");
+      }
+      if (event.code == "KeyP") {
+        addInput("text");
+      }
+      if (event.code == "KeyU") {
+        addInput("list");
+      }
+      if (event.code == "KeyO") {
+        addInput("list_number");
       }
     }
 
     if (event.target.tagName == "INPUT") {
-      console.log(event);
       if (event.code == "Enter") {
-        let title = document.createElement("h2");
-        title.className = "title";
-        title.textContent = event.target.value;
-        container.insertAdjacentElement("beforeend", title);
-        container.removeChild(event.target);
+        let className = event.target.classList[1];
+        addElement(className);
       }
     }
   });
