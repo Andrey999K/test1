cebf5837332ba42bc7b61392f3eb51ae1e7c8466
Andrey999K <andrey1234xxxx@mail.ru>
2020-09-09 12:03:49 +0300

    09.09.2020 12:03

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const container = document.querySelector(".container");
+  let end1 = true;
 
   const addInput = (element) => {
     container.insertAdjacentHTML(
@@ -9,7 +10,8 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector(`input.${element}`).focus();
   };
 
-  const addElement = (className) => {
+  const addElement = (className, end = true) => {
+    let input = document.querySelector(".input");
     if (className == "title") {
       tag = "h2";
     }
@@ -22,43 +24,93 @@ document.addEventListener("DOMContentLoaded", () => {
     if (className == "list_number") {
       tag = "ol";
     }
+    if (className == "list__item") {
+      tag = "li";
+    }
     let element = document.createElement(tag);
     if (tag == "ul" || tag == "ol") {
       let li = document.createElement("li");
-      li.textContent = event.target.value;
-      console.log(li);
-      element.insertAdjacentElement("beforeend", li);
+      li.className = "list__item";
+      if (input.value == "") {
+        container.removeChild(input);
+        end = true;
+      } else {
+        li.textContent = input.value;
+        element.insertAdjacentElement("beforeend", li);
+        input.value = "";
+        end = false;
+      }
+    } else if (tag == "li") {
+      element = document.querySelectorAll("ol, ul")[
+        document.querySelectorAll("ol, ul").length - 1
+      ];
+      console.log(element);
+      let li = document.createElement("li");
+      li.className = "list__item";
+      if (input.value == "") {
+        container.removeChild(input);
+        end = true;
+      } else {
+        li.textContent = input.value;
+        element.insertAdjacentElement("beforeend", li);
+        input.value = "";
+        end = false;
+      }
     } else {
       element.className = className;
-      element.textContent = event.target.value;
+      element.textContent = input.value;
+      container.removeChild(input);
+      end = true;
     }
     container.insertAdjacentElement("beforeend", element);
-    container.removeChild(event.target);
+    return end;
   };
 
   document.addEventListener("keydown", () => {
-    if (event.ctrlKey == true) {
-      if (event.code != "F5") {
-        event.preventDefault();
-      }
-      if (event.code == "KeyH") {
-        addInput("title");
-      }
-      if (event.code == "KeyP") {
-        addInput("text");
-      }
-      if (event.code == "KeyU") {
-        addInput("list");
-      }
-      if (event.code == "KeyO") {
-        addInput("list_number");
+    if (event.target.tagName != "INPUT") {
+      if (event.ctrlKey == true) {
+        if (event.code != "F5") {
+          event.preventDefault();
+        }
+        if (event.code == "KeyH") {
+          addInput("title");
+        }
+        if (event.code == "KeyP") {
+          addInput("text");
+        }
+        if (event.code == "KeyU") {
+          addInput("list");
+        }
+        if (event.code == "KeyO") {
+          addInput("list_number");
+        }
       }
     }
 
     if (event.target.tagName == "INPUT") {
       if (event.code == "Enter") {
         let className = event.target.classList[1];
-        addElement(className);
+        if (end1) {
+          end1 = addElement(className);
+        } else {
+          end1 = addElement("list__item");
+        }
+      }
+
+      if (event.code == "Escape") {
+        container.removeChild(event.target);
+      }
+    }
+  });
+
+  document.addEventListener("click", () => {
+    if (event.target.className != "input") {
+      let end1 = true;
+      let className = document.querySelector(".input").classList[1];
+      if (end1) {
+        end1 = addElement(className);
+      } else {
+        end1 = addElement("list__item");
       }
     }
   });
